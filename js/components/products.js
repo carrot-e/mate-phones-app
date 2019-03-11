b60ec4f1b2d318e5ef853eb3003577f9be14a0a6
dina.pivaeva <dina.pivaeva@auroraglobal.com>
2019-03-11 14:44:14 +0200

    released sort method

--- js/components/products.js
+++ js/components/products.js
@@ -6,14 +6,14 @@ import ProductList from './product-list.js';
 import PhoneService from '../services/phone-service.js';
 
 class Products extends BaseComponent {
-  constructor(params) {
-    super(params);
+    constructor(params) {
+        super(params);
 
-    this._initComponents();
-  }
+        this._initComponents();
+    }
 
-  _render() {
-    this._element.innerHTML = `
+    _render() {
+        this._element.innerHTML = `
       <div class="row">
         <div class="col-md-2">
           <section>
@@ -26,36 +26,41 @@ class Products extends BaseComponent {
         <div class="col-md-10" data-component="product-list"></div>
       </div>
     `;
-  }
+    }
 
-  _initComponents() {
-    this._search = new Search({
-      element: this._element.querySelector('[data-component="search"]'),
-      parent: this,
-    });
+    _initComponents() {
+        this._search = new Search({
+            element: this._element.querySelector('[data-component="search"]'),
+            parent: this,
+        });
 
-    this._sort = new Sort({
-      element: this._element.querySelector('[data-component="sort"]'),
-    });
+        this._sort = new Sort({
+            element: this._element.querySelector('[data-component="sort"]'),
+            parent: this,
+        });
 
-    this._shoppingCart = new ShoppingCart({
-      element: this._element.querySelector('[data-component="shopping-cart"]'),
-    });
+        this._shoppingCart = new ShoppingCart({
+            element: this._element.querySelector('[data-component="shopping-cart"]'),
+        });
 
-    this._productList = new ProductList({
-      element: this._element.querySelector('[data-component="product-list"]'),
-      products: PhoneService.getAll(),
-      parent: this,
-    });
-  }
+        this._productList = new ProductList({
+            element: this._element.querySelector('[data-component="product-list"]'),
+            products: PhoneService.getAll(),
+            parent: this,
+        });
+    }
 
-  searchUpdated(query) {
-    this._productList.products = PhoneService.getFiltered(query);
-  }
+    searchUpdated(query) {
+        this._productList.products = PhoneService.getFiltered(query);
+    }
 
-  phoneSelected(phoneId) {
-    this._parent.phoneSelected(phoneId);
-  }
+    sortUpdated(key) {
+        this._productList.products = PhoneService.getSorted(key);
+    }
+
+    phoneSelected(phoneId) {
+        this._parent.phoneSelected(phoneId);
+    }
 }
 
 export default Products;
--- js/services/phone-service.js
+++ js/services/phone-service.js
@@ -1,19 +1,42 @@
 import phones from '../../phones/phones.js';
 
 class PhoneService {
-  static getAll() {
-    return phones;
-  }
+    static getAll() {
+        return phones;
+    }
 
-  static getFiltered(query) {
-    return phones.filter(phone => {
-      return phone.name.toLowerCase().includes(query.toLowerCase());
-    });
-  }
+    static getFiltered(query) {
+        return phones.filter(phone => {
+            return phone.name.toLowerCase().includes(query.toLowerCase());
+        });
+    }
 
-  static async getPhone(id) {
-    return (await import(`../../phones/${id}.js`)).default;
-  }
+    static async getPhone(id) {
+        return (await import(`../../phones/${id}.js`)).default;
+    }
+
+    static getSorted(key) {
+        if (key === 'age') {
+            return phones.sort(function (a, b) {
+                return a.age - b.age;
+            });
+        }
+
+        if (key === 'name') {
+
+            return phones.sort(function (a, b){
+                if (a.name < b.name) {
+                    return -1;
+                }
+
+                if (a.name > b.name) {
+                    return 1;
+                }
+
+                return 0;
+            });
+        }
+    }
 }
 
 export default PhoneService;
